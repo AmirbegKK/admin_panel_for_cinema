1126de164e81b502fddfff5c235c66f1e8c3b24d
Amirbeg Kurbanov <avastfsoc541@gmail.com>
2022-02-18 16:43:11 +0300

    Add additional indexes and default geanarated uuids

--- 01_schema_design/db_schema.sql
+++ 01_schema_design/db_schema.sql
@@ -1,9 +1,10 @@
 -- Создание отдельной схемы для контента:
 CREATE SCHEMA IF NOT EXISTS content;
+CREATE EXTENSION "pgcrypto";
 
 -- Жанры кинопроизведений: 
 CREATE TABLE IF NOT EXISTS content.genre (
-    id uuid PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     name text NOT NULL,
     description text,
     created timestamp with time zone,
@@ -12,7 +13,7 @@ CREATE TABLE IF NOT EXISTS content.genre (
 
 -- Участники фильмов:
 CREATE TABLE IF NOT EXISTS content.person (
-    id uuid PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     full_name text NOT NULL,
     created timestamp with time zone,
     modified timestamp with time zone
@@ -20,7 +21,7 @@ CREATE TABLE IF NOT EXISTS content.person (
 
 -- Информация о фильмах:
 CREATE TABLE IF NOT EXISTS content.film_work (
-    id uuid PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     title text NOT NULL,
     description text,
     creation_date DATE,
@@ -32,20 +33,24 @@ CREATE TABLE IF NOT EXISTS content.film_work (
 
 -- Информация о фильмах и жанрах:
 CREATE TABLE IF NOT EXISTS content.genre_film_work (
-    id uuid PRIMARY KEY,
-    genre_id uuid NOT NULL,
-    film_work_id uuid NOT NULL,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    genre_id uuid NOT NULL REFERENCES content.genre,
+    film_work_id uuid NOT NULL REFERENCES content.film_work,
     created timestamp with time zone
 );
 
 -- Информация об актерах и фильмах:
 CREATE TABLE IF NOT EXISTS content.person_film_work (
-    id uuid PRIMARY KEY,
-    person_id uuid NOT NULL,
-    film_work_id uuid NOT NULL,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    person_id uuid NOT NULL REFERENCES content.person,
+    film_work_id uuid NOT NULL REFERENCES content.film_work,
     role text NOT NULL,
     created timestamp with time zone
 );
 
+CREATE INDEX genre_name_idx ON content.genre (name)
+CREATE INDEX filmwork_type_idx ON content.film_work (type)
+CREATE INDEX filmwork_creation_date_idx ON content.film_work (creation_date)
+CREATE INDEX filmwork_rating_idx ON content.film_work (rating)
 CREATE UNIQUE INDEX film_work_person ON content.person_film_work (film_work_id, person_id, role); 
 CREATE UNIQUE INDEX film_work_genre ON content.genre_film_work (film_work_id, genre_id);
